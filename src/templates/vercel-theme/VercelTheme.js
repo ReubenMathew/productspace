3b3c9c6b1711a91c7155ce4694cdf9da35372c72
Reuben Mathew <intelphysic@gmail.com>
2020-06-07 18:21:05 -0400

    splash responsiveness for medium sized screens

--- src/templates/vercel-theme/VercelTheme.js
+++ src/templates/vercel-theme/VercelTheme.js
@@ -3,18 +3,17 @@ import Nav from './components/Nav'
 import Splash from './components/Splash'
 import Markdown from './components/Markdown'
 
-const splashData = {
+const data = {
   tagline: 'Brainstorm. Organize. Create.',
   description: 'VennFX is a cross-platform Venn Diagram desktop application built for performance and customization.',
   download: 'https://github.com/EECS2311-Team8/SoftwareDevelopmentProject/releases/tag/vF',
-  source: 'https://github.com/ReubenMathew/VennFX'
-}
-
-const navData = {
+  source: 'https://github.com/ReubenMathew/VennFX',
   projectName: 'VennFX',
   docs: "https://github.com/ReubenMathew/VennFX/tree/master/Documentation",
   author: 'Reuben Ninan'
 }
+
+
 
 function VercelTheme(){
     
@@ -24,15 +23,15 @@ function VercelTheme(){
         {/* fixed class for sticky nav */}
         <div className="bg-accent1">
           <Nav 
-            projectName={`${navData.projectName}`}
-            docs={`${navData.docs}`}
-            author={`${navData.author}`}
+            projectName={`${data.projectName}`}
+            docs={`${data.docs}`}
+            author={`${data.author}`}
           />
           <Splash
-            tagline={`${splashData.tagline}`}
-            description={`${splashData.description}`}
-            download={`${splashData.download}`}
-            source={`${splashData.source}`}
+            tagline={`${data.tagline}`}
+            description={`${data.description}`}
+            download={`${data.download}`}
+            source={`${data.source}`}
           />
         </div>
         <Markdown/>
