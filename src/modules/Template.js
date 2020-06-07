d7eeea40530a33e68cff8c31d5007e47c85966f4
Reuben Mathew <intelphysic@gmail.com>
2020-06-07 19:46:55 -0400

    conditional download button rendering

--- src/modules/Template.js
+++ src/modules/Template.js
@@ -5,13 +5,23 @@ const themes = {
   'vercel': VercelTheme,  
 }
 
-function Template(props){
+const data = {
+  tagline: 'Brainstorm. Organize. Create.',
+  description: 'VennFX is a cross-platform Venn Diagram desktop application built for performance and customization.',
+  download: 'https://github.com/EECS2311-Team8/SoftwareDevelopmentProject/releases/tag/vF',
+  source: 'https://github.com/ReubenMathew/VennFX',
+  projectName: 'VennFX',
+  docs: "https://github.com/ReubenMathew/VennFX/tree/master/Documentation",
+  author: 'Reuben Ninan'
+}
+
+function Template(props) {
 
     const Theme = themes[props.theme];
   
     return (
       <div>       
-        <Theme/>
+        <Theme {...data}/>
       </div>
     );
   
--- src/templates/vercel-theme/VercelTheme.js
+++ src/templates/vercel-theme/VercelTheme.js
@@ -3,19 +3,7 @@ import Nav from './components/Nav'
 import Splash from './components/Splash'
 import Markdown from './components/Markdown'
 
-const data = {
-  tagline: 'Brainstorm. Organize. Create.',
-  description: 'VennFX is a cross-platform Venn Diagram desktop application built for performance and customization.',
-  download: 'https://github.com/EECS2311-Team8/SoftwareDevelopmentProject/releases/tag/vF',
-  source: 'https://github.com/ReubenMathew/VennFX',
-  projectName: 'VennFX',
-  docs: "https://github.com/ReubenMathew/VennFX/tree/master/Documentation",
-  author: 'Reuben Ninan'
-}
-
-
-
-function VercelTheme(){
+function VercelTheme(props){
     
   return (
     <div>
@@ -27,15 +15,15 @@ function VercelTheme(){
         {/* fixed class for sticky nav */}
         <div className="bg-accent1">
           <Nav 
-            projectName={`${data.projectName}`}
-            docs={`${data.docs}`}
-            author={`${data.author}`}
+            projectName={`${props.projectName}`}
+            docs={`${props.docs}`}
+            author={`${props.author}`}
           />
           <Splash
-            tagline={`${data.tagline}`}
-            description={`${data.description}`}
-            download={`${data.download}`}
-            source={`${data.source}`}
+            tagline={`${props.tagline}`}
+            description={`${props.description}`}
+            download={`${props.download}`}
+            source={`${props.source}`}
           />
         </div>
         <Markdown/>
