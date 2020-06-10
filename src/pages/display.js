787926019c2f93d71a931a1a861dde8b99f8a745
Reuben Mathew <intelphysic@gmail.com>
2020-06-10 16:17:42 -0400

    page create form init

---
+++ src/pages/display.js
@@ -0,0 +1,15 @@
+import Template from '../modules/Template'
+
+function Display(){
+  
+  return (
+    <div>
+      <Template theme="vercel"/>
+    </div>
+  );
+
+}
+
+
+
+export default Display;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
-import Template from '../modules/Template'
+import Link from 'next/link'
 
 function Home(){
   
   return (
     <div>
-      <Template theme="vercel"/>
+      <div>
+        <Link href="/display">Template</Link>
+      </div>
+      <div>
+        <Link href="/setup">Setup</Link>
+      </div>
     </div>
-  );
-
+    );
 }
-
-
 
 export default Home;
