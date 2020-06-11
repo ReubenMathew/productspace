6f0f7293ada8ebb13d44b3748f2b5232c83d65c5
Reuben Ninan <intelphysic@gmail.com>
2020-06-10 23:43:45 -0400

    setup page updates

--- src/pages/setup.js
+++ src/pages/setup.js
@@ -1,24 +1,31 @@
 
-function Short() {
+function Short(props) {
+    
     return (
-        <div class="w-full px-3">
-            <input placeholder="Text here" class="px-4 py-3 rounded-lg border border-accent2 focus:border-accent5 transition duration-200 ease-in-out"/>
+        <div class="py-4">
+            <label className="px-2 py-2">{props.label}</label>
+            <input placeholder={props.text} class="px-4 py-2 focus:outline-none rounded-lg border border-accent2 focus:border-accent5 transition duration-200 ease-in-out"/>
+        </div>
+    )
+}
+
+function Medium(props) {
+    
+    return (
+        <div class="">
+            <label className="px-2 py-2">{props.label}</label>
+            <input placeholder={props.text} class="px-4 py-2 min-w-lg focus:outline-none rounded-lg border border-accent2 focus:border-accent5 transition duration-200 ease-in-out"/>
         </div>
     )
 }
 
 function Setup(){
     return (
-        <div class="bg-accent1 min-h-screen flex  items-center justify-center">
-            <Short/>
-            {/* <div class="w-full px-3">
-                Hello
-                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                    Password
-                </label>
-                <input class="appearance-none block max-w-md bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************"/>
-                <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
-            </div> */}
+        <div class="bg-accent1 min-h-screen px-8 flex flex-wrap items-center justify-center">
+            <div class="flex flex-wrap flex-col justify-center ">
+                <Short label="Project Name" text="VennFX"/>
+                <Short label="Description" text="Write a description"/>
+            </div>            
         </div>
     );
 }
