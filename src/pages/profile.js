b6cbd79051c0a6b67d43cd86e686c1f5d7ac625d
Reuben Ninan <intelphysic@gmail.com>
2020-06-12 00:03:34 -0400

    site profile cards complete

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,11 +1,43 @@
 import ProjectCard from '../modules/profile/ProjectCard'
 
+const data = [
+    {
+        source: 'https://github.com/ReubenMathew/VennFX',
+        projectName: 'VennFX'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/productspace',
+        projectName: 'productspace'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/pingo',
+        projectName: 'pingo'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/SodaDB',
+        projectName: 'SodaDB'
+    },
+
+]
+
+
+function Cards(){
+    return(
+        <div class="flex flex-wrap ">
+            {data.map(item => (
+                <ProjectCard 
+                    source={item.source}
+                    projectName={item.projectName}
+                />
+            ))}
+        </div>
+    )
+}
+
 function Profile() {
     return (
-        <div class="bg-white min-h-screen px-8 sm:px-32 py-8 flex flex-wrap flex-col items-center justify-center">
-            <ProjectCard/>
-            <ProjectCard/>
-            <ProjectCard/>
+        <div class="bg-white min-h-screen px-8 py-8  ">
+            <Cards/>
         </div>
     );
 }
