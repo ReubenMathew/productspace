5e006a6ee09bd6855b07ac917aa75dc77905d9fb
Reuben Mathew <intelphysic@gmail.com>
2020-06-22 17:02:18 -0400

    data made it through to theme

--- src/templates/vercel-theme/VercelTheme.js
+++ src/templates/vercel-theme/VercelTheme.js
@@ -3,7 +3,12 @@ function VercelTheme(props){
   console.log("Vercel Theme",props)
   return (
     <div>
-      Hello
+      <div>{props.ProjectName}</div>
+      <div>{props.Tagline}</div>
+      <div>{props.Description}</div>
+      <div>{props.Download}</div>
+      <div>{props.Source}</div>
+      <div>{props.Docs}</div>
     </div>
   );
 }
