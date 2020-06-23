1d655dc7b2a29569f3807ad49b7850e8121ab928
Reuben Mathew <intelphysic@gmail.com>
2020-06-23 17:05:07 -0400

    profile page display project cards

---
+++ src/pages/[user]/index.js
@@ -0,0 +1,45 @@
+import ProjectCard from '../modules/profile/ProjectCard'
+
+const data = [
+    {
+        source: 'https://github.com/ReubenMathew/VennFX',
+        projectName: 'VennFX'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/productspace',
+        projectName: 'productspace'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/pingo',
+        projectName: 'pingo'
+    },
+    {
+        source: 'https://github.com/ReubenMathew/SodaDB',
+        projectName: 'SodaDB'
+    },
+
+]
+
+
+function Cards(){
+    return(
+        <div class="flex flex-wrap ">
+            {data.map(item => (
+                <ProjectCard 
+                    source={item.source}
+                    projectName={item.projectName}
+                />
+            ))}
+        </div>
+    )
+}
+
+function Profile() {
+    return (
+        <div class="bg-white min-h-screen px-8 py-8  ">
+            <Cards/>
+        </div>
+    );
+}
+
+export default Profile;
