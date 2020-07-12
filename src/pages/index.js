3e0adcd5918833703f98dc39043c6d613991d69b
Reuben Mathew <intelphysic@gmail.com>
2020-07-11 20:01:20 -0400

    clear index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,7 @@ function Home(){
   
   return (
     <div>
-      <div class="min-h-screen flex flex-col items-center justify-center text-accent7">
-        <div class="font-bold text-5xl">
-          coming soon
-        </div>
-        <div class="text-xl">
-          checkout a <a href="/Chidalu" class="text-coral transition duration-300 hover:bg-coral hover:text-white">demo</a>
-        </div>
-      </div>
+
     </div>
     );
 }
